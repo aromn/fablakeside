472da040506b094c7b48c3f10bac11f273477e5e
Aldo Romero <aldoromnn@gmail.com>
2020-11-10 14:49:41 -0600

    database seeder:
    Added Clients and User model classes.
    Created User and 10 test clients.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Clients;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+    
+    	Clients::factory()->times(10)->create();
+        
     }
 }
