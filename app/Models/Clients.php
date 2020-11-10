8bdb20dc372f89a816da4f678fd87aa84d7e9706
Aldo Romero <aldoromnn@gmail.com>
2020-11-10 14:49:06 -0600

    clients model:
    Created boot function.

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Clients extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["business_name", "business_category"];
+
+    protected static function boot()
+    {
+    	parent::boot();
+    	self::creating(function($table){
+    		if ( ! app()->runningInConsole()){
+    			$table->user_id = auth()->id();
+    		}
+    	});
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
 }
