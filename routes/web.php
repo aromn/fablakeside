fa581208a7c39be90e9a0b5f30e55db218578a76
Aldo Romero <aldoromnn@gmail.com>
2020-11-11 10:28:23 -0600

    Routes => web: changed Httd to Http, added clients route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Httd\Controllers\ClientsController;
+use App\Http\Controllers\ClientsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clients', 'App\Http\Controllers\ClientsController@index');
+
 
 Route::resource("clients", ClientsController::class);
