ad7ac394f6bed5203505a7c864f36cfc65d86433
Aldo Romero <aldoromnn@gmail.com>
2020-11-11 12:09:18 -0600

    Models => Clients:
    Added Category model class.
    Created function category() that returns categories related to the
    clients.

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Clients extends Model
 {
@@ -24,6 +25,11 @@ class Clients extends Model
     public function user()
     {
     	return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 
 }
