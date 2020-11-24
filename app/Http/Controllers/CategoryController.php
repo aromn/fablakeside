6fea21d31c058bd1ef284d004488197470b085f3
Aldo Romero <aldoromnn@gmail.com>
2020-11-24 11:14:45 -0600

    Controllers => Category: Created index function.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+	public function index()
+	{
+		$categories = 'App\Models\Category'::all();
+		return view("categories.index", ['categories' => $categories]);
+	}
+
 	public function subcategories($id)
 	{
 		$subcategories = 'App\Models\Category'::where('parent_id', '=', $id)->get();
