d95aa8cca45647a8a3b1a2cc238a1f3e0e614a16
NahualliArt <artnahualli@gmail.com>
2020-11-24 17:55:10 -0600

    Routes => Web: Added Routes to HTML Pages

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,24 @@ Route::get('/business_categories', 'App\Http\Controllers\CategoryController@inde
 
 Route::resource("clients", ClientsController::class);
 Route::get('/search', 'App\Http\Controllers\ClientsController@search');
+
+/* Routes to HTML Pages */
+Route::get('home', function () {
+    return view('home');
+})->name('home1');
+
+Route::get('specialBusiness', function () {
+    return view('specialBusiness');
+})->name('specialBusiness1');
+
+Route::get('listBusiness', function () {
+    return view('listBusiness');
+})->name('listBusiness1');
+
+Route::get('ourTeam', function () {
+    return view('ourTeam');
+})->name('ourTeam1');
+
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact1');
