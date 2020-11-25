5c71db5c7424179a1d3289b1ece93e6fd6b818b2
NahualliArt <artnahualli@gmail.com>
2020-11-24 18:31:06 -0600

    Layouts => Added <head> at Header and Footer Master

---
+++ resources/views/layouts/header-footer-master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Por defecto -->
+    <title>Find a Business Ajijic</title>
+    <!--Favicon-->
+    <link rel="icon" 
+      type="image/png" 
+      href="#">
+
+    <!--DESCRIPCION PARA GOOGLE-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="We Make it Easy for Both Businesses and Customers">
+    <meta name="author" content="FAB Ajijic by Alex Sealers">
+
+    <!--FONT :: Poppins-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+    <!--EDGE NAVEGADOR-->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Vendor - Bootstrap CSS Files -->
+    <link href="{{ asset('vendor/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('vendor/line-awesome/css/line-awesome.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet" type="text/css" >
+    <!-- Propios estilos -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
+    
+</head>
+<body>
+</body>
+</html>
