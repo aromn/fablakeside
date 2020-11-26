b33708c6fa74456ce0b792519e4d641f1b3c15fd
NahualliArt <artnahualli@gmail.com>
2020-11-25 18:18:20 -0600

    Views => Home, Added Search section button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,14 @@
       <h1>FIND A BUSINESS AJIJIC</h1>
       <p>Webpage Management for Business Owners <br> and One-Stop Portal Costumers</p>
     </div>
+</div>
+
+<!--Search Section Button-->
+<div class="container subtitle w-75 shadow align-self-center">
+    <div class="text-center py-2">
+      <!--THIS ONE GOES TO THE ALL BUSINESS SEARCH PAGE-->
+      <h5>SEARCH FOR <a href="#" class="subtitle-link">THE BEST BUSINESS</a> FOR YOU</h5>
+    </div>
   </div>
 
 @endsection
