af8cf13f58ddcb362b1c0a468183af334a102d37
NahualliArt <artnahualli@gmail.com>
2020-11-25 18:20:46 -0600

    Views => Home, Added Categories Selection
    Check! Data Filters seems not to work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,35 @@
       <!--THIS ONE GOES TO THE ALL BUSINESS SEARCH PAGE-->
       <h5>SEARCH FOR <a href="#" class="subtitle-link">THE BEST BUSINESS</a> FOR YOU</h5>
     </div>
-  </div>
+</div>
+
+<!-- ======= SPECIAL BUSINESS SECTION ======= -->
+<section class="section site-portfolio">  
+    <div class="container">
+
+        <!--CATEGORIES SELECTION-->
+        <div class="row mb-5 align-items-center">
+            <div class="col-md-12 col-lg-12 text-center text-lg-center">
+                <div id="filters" class="filters">
+                    <a href="#" data-filter="*" class="active">ALL</a>
+                    <a href="#" data-filter=".restaurants">RESTAURANTS</a>
+                    <a href="#" data-filter=".markets">MARKETS</a>
+                    <a href="#" data-filter=".entertainment">ENTERTAINMENT</a>
+                    <a href="#" data-filter=".arts">ARTS</a>
+                    <a href="#" data-filter=".shopping">SHOPPING</a>
+                    <a href="#" data-filter=".insurance">INSURANCE</a>
+                    <a href="#" data-filter=".house">HOUSE</a>
+                    <a href="#" data-filter=".pets">PETS</a>
+                    <a href="#" data-filter=".healthCare">HEALTH CARE</a>
+                    <a href="#" data-filter=".autoSales">AUTO SALES</a>
+                    <!--THIS ONE GOES TO THE ALL BUSINESS SEARCH PAGE-->
+                    <a href="#" data-filter=".more">MORE</a>
+                </div>
+            </div>
+        </div>
+        <!--END CATEGORIES SELECTION-->
+
+    </div>
+</section>
 
 @endsection
