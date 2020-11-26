25c8fa3fd90f3486ac8ba2be81a92df95d7c24b4
NahualliArt <artnahualli@gmail.com>
2020-11-25 18:17:35 -0600

    Views => Home, Added Home Hero Image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.header-footer-master') 
+
+@section('section')
+
+<!--HOME HERO IMAGE-->
+<div class="container ml-auto text-center heroImg">
+    <div class="backImg">
+      <h1>FIND A BUSINESS AJIJIC</h1>
+      <p>Webpage Management for Business Owners <br> and One-Stop Portal Costumers</p>
+    </div>
+  </div>
+
+@endsection
