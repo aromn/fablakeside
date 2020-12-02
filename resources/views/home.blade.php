4ccec5a2459a7c6af49c9ee6610238aa43d4edbd
NahualliArt <artnahualli@gmail.com>
2020-12-02 16:35:19 -0600

    Views => Added Buisness Categories link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
 <div class="container subtitle w-75 shadow align-self-center">
     <div class="text-center py-2">
       <!--THIS ONE GOES TO THE ALL BUSINESS SEARCH PAGE-->
-      <h5>SEARCH FOR <a href="#" class="subtitle-link">THE BEST BUSINESS</a> FOR YOU</h5>
+      <h5>SEARCH FOR <a href="{{ route('specialBusiness1') }}" class="subtitle-link">THE BEST BUSINESS</a> FOR YOU</h5>
     </div>
 </div>
 
