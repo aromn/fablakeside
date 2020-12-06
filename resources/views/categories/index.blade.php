c2b614276ba8b75f40dd80a25b6ffbc79ec79e69
Aldo Romero <aldoromnn@gmail.com>
2020-12-05 20:08:45 -0600

    Views => Business categories:
    Added option "All" in left menu.
    Added category attribute to a links of left menu.
    Added class table-clients to the table.
    Added on click function of left menu options with ajax request.

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -22,7 +22,7 @@
       </div>
     </div>
   </div>
-  
+ 
   <!--CATEGORIES RESULTS-->
   <div class="container">
   <div class="row">
@@ -32,9 +32,12 @@
         <h5 class="my-4">Business Categories</h5>
         <span class="line"></span>
         <div class="nav flex-column nav-pills searchList" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <a class="nav-link active" category="All" id="v-pills-home-tab" data-toggle="pill" href="{{ route('specialBusiness1') }}" aria-selected="true">
+                All
+              </a>
           @foreach($categories as $category)
 			      @if($category->parent_id == 0)
-				<a class="nav-link" id="v-pills-home-tab" data-toggle="pills" href="{{ url('business_categories', $category->slug) }}" aria-selected="true">
+				<a class="nav-link"  category="{{ $category->slug }}" id="v-pills-home-tab" data-toggle="pill" href="{{ url('business_categories', $category->slug) }}" aria-selected="true">
 				        {{$category->name }}
 			        </a>
 		        @endif
@@ -44,10 +47,10 @@
         <h5 class="my-4">More Specific</h5>
         <span class="line"></span>  
         <div class="nav flex-column nav-pills searchList" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-          @foreach($categories as $category)
-			      @if($category->parent_id == 0)
-				      <a class="nav-link" id="v-pills-home-tab" data-toggle="pills" href="{{ url('business_categories', $category->slug) }}" aria-selected="true">
-				        {{$category->name }}
+          @foreach($subcategories as $subcategory)
+			      @if($subcategory->parent_id == 0)
+				      <a class="nav-link" id="v-pills-home-tab"  data-toggle="pills" href="{{ url('business_categories', $category->slug) }}" aria-selected="true">
+				        {{$subcategory->name }}
 			        </a>
 		        @endif
       	@endforeach
@@ -58,7 +61,7 @@
 <!--SHOW RESULTS FROM SPECIFIC SEARCH-->
     <div class="col-lg-9 mt-4">
    		<div class="row">
-<table class="table">
+<table class="table table-clients">
   <thead class="thead-dark">
     <tr>
       <th scope="col">Business name</th>
@@ -95,7 +98,28 @@
     <a href="#" class="buttonList">Learn How</a>
   </div>
 </div>
-  
+   <script type="text/javascript">
+    $('.table-clients').DataTable();
+    $('#v-pills-tab > a').on('click', function(e){
+        console.log($(this)[0].attributes.category.value);
+        // Avoid reloading the page.
+        e.preventDefault();
+
+        // Removing last results of the table's body.
+        $('.table-clients tbody').empty();
+        $.ajax({
+          url: '/business_categories/' + $(this)[0].attributes.category.value,
+          type: 'GET',
+          success: function(e){
+            $.each(e, function(i, v){
+              $('.table-clients').append('<tr><td>' + v.name + '</td><td>' + v.category + '</td><td>' + v.subcategory + '</td><td>' + v.location + '</td></tr>');
+            });
+            
+          }
+        });
+
+    });
+  </script>
   <script type="text/javascript"></script>
 
 </div>
