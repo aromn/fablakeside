989fc99a5c14963029bfaf439997a1bd46e2f21c
Aldo Romero <aldoromnn@gmail.com>
2020-12-05 20:04:47 -0600

    Controllers => Category:
    Added subcategories variable and send it to view.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,12 +9,13 @@ class CategoryController extends Controller
 {
 	public function index()
 	{
-        	$categories = 'App\Models\Category'::with("childs")->where('parent_id', 0)->get(); 
+        $categories = 'App\Models\Category'::with("childs")->where('parent_id', 0)->get(); 
+        $subcategories = 'App\Models\Category'::where('parent_id', '!=', 0)->get(); 
 		$clients = DB::table('clients')->select(DB::raw('business_name as name, location, category_id, subcategory_id, c1.name as category, c2.name as subcategory'))
 			->leftJoin('categories as c1', 'c1.id', 'clients.category_id')
 			->leftJoin('categories as c2', 'c2.id', 'clients.subcategory_id')
 			->get();
-		return view("categories.index", ['categories' => $categories, 'clients' => $clients]);
+		return view("categories.index", ['categories' => $categories, 'subcategories' => $subcategories, 'clients' => $clients]);
 	}
 
 	public function subcategories($id)
