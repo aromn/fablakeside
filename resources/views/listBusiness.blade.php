34609a7022365471309dde9a42d446aff010fbb5
NahualliArt <artnahualli@gmail.com>
2020-12-15 10:14:06 -0600

    Views => lisBusiness,  Added Nav  Bar and Section Yield sections

--- resources/views/listBusiness.blade.php
+++ resources/views/listBusiness.blade.php
@@ -1,1 +1,34 @@
-@extends('layouts.header-footer-master') 
+@extends('layouts.master') 
+
+@section('NavBar')
+<nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarResponsive" >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('home1') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('specialBusiness1') }}">Business Categories</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link active" href="{{ route('listBusiness1') }}">List Your Business</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('ourTeam1') }}">Our Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('contact1') }}">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+@endsection
+
+@section('section')
+
+@endsection
