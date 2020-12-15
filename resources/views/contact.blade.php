b8f4cfd99fec5b727b9cde69e3b7c0a52068fb0b
NahualliArt <artnahualli@gmail.com>
2020-12-15 10:42:48 -0600

    Views => contact, Added Subtitle section with Business Categories Link

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -40,4 +40,12 @@
   </div>
 </div>
 
+<!--Subtitle-->
+<div class="container subtitle w-75 shadow align-self-center">
+  <div class="text-center py-2">
+    <!--THIS ONE GOES TO THE ALL BUSINESS SEARCH PAGE-->
+    <h5>SEARCH FOR <a href="{{ route('specialBusiness1') }}" class="subtitle-link">THE BEST BUSINESS</a> FOR YOU</h5>
+  </div>
+</div>
+
 @endsection
