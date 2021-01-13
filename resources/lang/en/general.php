d4b6bccdb1b41215e6096c297683a0bcd7c88e71
Aldo Romero <aldoromnn@gmail.com>
2021-01-13 16:26:04 -0600

    Localization: Added translation for List Your Business page.

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -26,7 +26,90 @@ return [
 	'learn_how'				=> "Learn how",
 	'we_make_it_easy_msg'	=> "We Make it Easy for Both Businesses and Customers", 
 	'have_you_been_frustrated' => "Have you been frustrated by the lack of businesses with webpages around Lakeside? We list businesses and manage their pages to ensure they are up to date, and all in one convenient location. You can search for businesses by category or look up a specific business to view their own external page or their professionally managed webpage through our portal.",
-	'sign_up_today'			=> "Sing Up Your Business Today!",
+	'sign_up_your_business'			=> "Sing Up Your Business Today!",
+
+	/* === List Your Business page === */
+
+	'be_part'					=> "Be part of the team",
+	'be_part2'					=> "and let a whole new buch of people to know your business",
+	'our_services_title'		=> "Our Services & Prices",
+	'our_services_text'			=> "We provide various options and packages because we want to make this service accessible to all businesses and we understand no two businesses and their specific needs are exactly the same.",
+	'your_business_would'		=> "Your business would be listed under a category that is specific to your business in our site's directory",
+
+	// common text
+	'we_will_build_you'			=> "We will build you",
+	'more_info'					=> 'More info',
+	'1_to_3_categories'			=> "You can list your business under 1 to 3 categories*",
+	'apollo_2020'				=> "*Businesses that signed up with the Apollo 2020 program prior to June 15th, 2020 will receive their first 3 months free",
+	'i_want_it'					=> "I want it",
+
+	// Package 1
+	'basic_package'				=> "Basic Listing Package",
+	'one_time_fee'				=> "One-time fee for site construction",
+	'custom_1_page'				=> "Custom 1 page site",
+	'listing_and_basic'			=> "Listing and Basic maintenece",
+	'most_businesses'			=> "Most businesses do not need a complicated, multipage website or the costs associated with one.",
+	'description'				=> " We will build you a custom 1-page site that is listed under a category that is 
+                        specific 
+                        to your business in our site's directory for the bargain price of $200 pesos a 
+                        month, 
+                        and you won't be charged 1 centavo before we sign 100 businesses to our site, 
+                        because 
+                        without the volume we really aren't providing you the service we sold you on. ",
+	'if_you_cant'				=> "If you can't pick just one category to list under we can help you there too. ",
+	'list_under'				=> "List under two categories for 350 a month or three for 500 a month. 
+                        (Businesses that signed up with the Apollo 2020 program prior to June 15th, 
+                        2020 will receive their first 3 months free).",
+	
+	// Package 2
+	'custom_site'				=> "Custom Site Construction and Maintenance",
+	'custom_1_page_package2'	=> "Custom 1 page site designed by our experts and included with membership in basic package",
+	'more_robust_site'			=> "More robust Site",
+	'you_need_more'				=> "You need more than 1 page site? You got it",
+	'perfect_for'				=> "Perfect for stores who want to sell online",
+	'monthly_maintenance'		=> "Monthly Maintenance",
+	'in_addition_to'			=> "In addition to a basic listing service, we also offer a service to build 
+			a more robust custom website for you if your needs are greater than we can 
+			provide on one single page.",
+	'if_you_are_a' 				=> "If you are a store interested in selling online or a medical practitioner 
+			who wants to provide the customers with the proper brand image before they even 
+			step into your office, this might be something for you.",
+	'we_will_custom_build' => "We will custom build your webpage, host it on our site, and maintain it 
+			for $500 pesos a month (construction fees are an extra, one-time fee and 
+			vary depending on what you need from your webpage and the time it will take to construct).",
+
+	// Featured listing
+	'featured_fee' 				=> "Featured Listing Fee",
+	'500_pesos'					=> "500 pesos one-time payment to be listed in the first row, 100 pesos less for each subsequent row with the final row costing 100 pesos.",
+
+	'100_signed'				=> "Once we have signed 100 businesses and officially launch we will offer you the option to promote your business on our home page.",
+	'20_slots_we_have'			=> "We will have 20 slots, arranged in rows of 4 with 5 rows total.",
+	'3_slots_to_sell'			=> "We will sell 3 slots in each row $500 pesos one-time payment to be listed in the first row, $100 pesos less for each subsequent row with the final row costing $100 pesos.",
+	'free_slot'					=> "We will be offering every business the potential to win a free slot in each row of the featured businesses each month.",
+	'smaller_businesses' 		=> "In order to support smaller businesses, we have 5 empty slots for featured businesses each month.",
+	'conduct_our_election'		=> "We will conduct our selection through excel's randomization tool, with the top 5 business appearing on top gaining a free slot for the next month in their corresponding position in rows 1-5 of our featured businesses.",
+	'conduct_the_drawing'		=> "Since we conduct the drawing a month ahead, this prevents business from potentially paying for a slot and then receiving it for free.",
+	'transparent_and_fair'		=> "Although this is a little complicated, our goal by doing this is to keep everything transparent and fair.",
+	'sign_up_today'				=> "SIGN UP TODAY!",
+	'call_us'					=> "Call Us and Join",
+	'any_questions'				=> "ANY QUESTIONS?",
+	'feel_free_to_call'			=> "Feel free to call us or message us on WhatsApp and we will be happy to help you!",
+	
+	// Contact Us form
+	'contact_us'				=> "Contact Us",
+	'full_name'					=> "Full Name",
+	'email'						=> 'Email',
+	'phone'						=> 'Phone',
+	'what_service'				=> "What service do you need?",
+	'what_product'				=> "What kind of products do you want sell?",
+	'digital_services_products' => "Digital Services or Products",
+	'physical_services_products' => "Psycical Services or Products",
+	'services_consulting'		=> "Services Consulting",
+	'other'						=> "Other",
+	'what_is_it'				=> "What is It?",
+	'message'					=> "Message",
+	'send'						=> "Send",
+
 
 	/* Layout app */
 	'select_location'			=> "Select a location",
--- resources/lang/es/general.php
+++ resources/lang/es/general.php
@@ -29,6 +29,65 @@ return [
 	'have_you_been_frustrated' => "¿Se ha sentido frustrado por la falta de empresas con páginas web en el área del Lago de Chapala? Enumeramos empresas y administramos sus páginas web para asegurarnos de que estén actualizadas y todo en una ubicación conveniente. Puede buscar empresas por categoría o buscar una empresa específica para ver su propia página externa o su página web administrada profesionalmente a través de nuestro portal.",
 	'sign_up_today'			=> "¡Registre su negocio hoy!",
 
+	/* === List Your Business page === */
+	'be_part'					=> "Se parte del equipo",
+	'be_part2'					=> "y hazle saber al mundo sobre tu negocio",
+	'our_services_title'		=> "Nuestros servicios y precios",
+	'our_services_text'			=> "Ofrecemos varias opciones y paquetes porque queremos que este servicio sea accesible para todas las empresas y entendemos que no hay dos empresas y sus necesidades específicas son exactamente las mismas.",
+	'your_business_would'		=> "Su negocio figuraría en una categoría específica de su negocio en el directorio de nuestro sitio.",
+
+	// common text
+	'we_will_build_you'			=> "Construiremos para ti",
+	'more_info'					=> 'Más información',
+	'1_to_3_categories'			=> "Puedes listar tu negocio de 1 a 3 categorias*",
+	'apollo_2020'				=> "*Las empresas que se inscribieron en el programa Apollo 2020 antes del 15 de junio de 2020 recibirán sus primeros 3 meses gratis",
+
+	// Package 1
+	'basic_package'				=> "Paquete de Listado básico",
+	'one_time_fee'				=> "Tarifa única para la construcción del sitio",
+	'custom_1_page'				=> "Sitio de 1 página personalizada",
+	'listing_and_basic'			=> "Listado y mantenimiento básico",
+	
+	// Package 2
+	'custom_site'				=> "Construcción y mantenimiento de sitios personalizados",
+	'custom_1_page_package2'	=> "Sitio personalizado de 1 página diseñado por nuestros expertos e incluido con la membresía en el paquete básico",
+	'more_robust_site'			=> "Sitio más robusto",
+	'you_need_more'				=> "¿Necesita más de una página? La tienes",
+	'perfect_for'				=> "Perfecto para tiendas que quieren vender en línea.",
+	'monthly_maintenance'		=> "Mantenimeinto mensual",
+
+	// Featured listing
+	'featured_fee' 				=> "Tarifa de listado destacado",
+	'500_pesos'					=> "Pago único de 500 pesos para ser puesto en la primera fila, 100 pesos menos por cada fila posterior y la última fila tendrá un costo de 100 pesos.",
+
+	'100_signed'				=> "Una vez que hayamos firmado 100 negocios y el lanzamiento oficial, le ofreceremos la opción de promocionar su negocio en nuestra página de inicio.",
+	'20_slots_we_have'			=> "Tendremos 20 espacios, dispuestos en filas de 5 con 4 columnas en total.",
+	'3_slots_to_sell'			=> "Venderemos 3 ranuras en cada fila $ 500 pesos de pago único que se enumerarán en la primera fila, $ 100 pesos menos por cada fila posterior y la última fila tendrá un costo de $ 100 pesos.",
+	'free_slot'					=> "Ofreceremos a todas las empresas la posibilidad de ganar un espacio gratis en cada fila de las empresas destacadas cada mes.",
+	'smaller_businesses' 		=> "Para ayudar a las empresas más pequeñas, tenemos 5 espacios vacíos para las empresas destacadas cada mes.",
+	'conduct_our_election'		=> "Realizaremos nuestra selección a través de la herramienta de aleatorización de Excel, y las 5 empresas principales que aparecen en la parte superior obtienen un espacio libre para el próximo mes en su posición correspondiente en las filas 1-5 de nuestras empresas destacadas.",
+	'conduct_the_drawing'		=> "Dado que realizamos el sorteo con un mes de anticipación, esto evita que las empresas paguen potencialmente por un espacio y luego lo reciban gratis.",
+	'transparent_and_fair'		=> "Aunque esto es un poco complicado, nuestro objetivo al hacer esto es mantener todo transparente y justo.",
+	'sign_up_today'				=> "¡REGÍSTRATE HOY!",
+	'call_us'					=> "Llámanos y únete",
+	'any_questions'				=> "¿ALGUNA PREGUNTA?",
+	'feel_free_to_call'			=> "¡No dude en llamarnos o enviarnos un mensaje en WhatsApp y estaremos encantados de ayudarle!",
+	
+	// Contact Us form
+	'contact_us'				=> "Contacta con nosotros",
+	'full_name'					=> "Nombre completo",
+	'email'						=> 'Email',
+	'phone'						=> 'Teléfono',
+	'what_service'				=> "Qué servicio necesitas?",
+	'what_product'				=> "Qué tipo de producto vendes?",
+	'digital_services_products' => "Servicios digital o productos",
+	'physical_services_products' => "Servicios físicos o productos",
+	'services_consulting'		=> "Servicios de consultoría",
+	'other'						=> "Otro",
+	'what_is_it'				=> "¿Qué es?",
+	'message'					=> "Mensaje",
+	'send'						=> "Enviar",
+
 	/* Layout app */
 	'select_location'			=> "Selecciona un lugar",
 	'select_category'			=> "Selecciona una categoría",
