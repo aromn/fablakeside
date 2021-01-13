910cf0a783adbf9ad373470c137080ac541d815c
Aldo Romero <aldoromnn@gmail.com>
2021-01-13 16:31:17 -0600

    Views: Removed main menu on each page.

--- resources/views/ourTeam.blade.php
+++ resources/views/ourTeam.blade.php
@@ -1,34 +1,5 @@
 @extends('layouts.master') 
 
-@section('NavBar')
-<nav class="navbar navbar-expand-lg menuMovil">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-md-center" id="navbarResponsive" >
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('home1') }}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('specialBusiness1') }}">Business Categories</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('listBusiness1') }}">List Your Business</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link active" href="{{ route('ourTeam1') }}">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('contact1') }}">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-@endsection
-
 @section('section')
 
 <!--HERO IMAGE-->
