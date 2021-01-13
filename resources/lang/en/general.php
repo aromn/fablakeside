5fea99c5a3ab3878b7c15b1b65604589532e0b4a
Aldo Romero <aldoromnn@gmail.com>
2021-01-13 13:34:42 -0600

    Localization: Renamed en and es files and added first translations.

---
+++ resources/lang/en/general.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+	/* === HOME PAGE ===*/
+	/* Main menu */
+	'home' => 'Home',
+	'business_categories' 	=> "Business Categories",
+	'list_your_business'  	=> "List Your Business",
+	'about_us'				=> "About Us",
+	'contact'				=> 'Contact',
+	'search'				=> 'Search',
+	/* Main banner */
+	"webpage_description" 	=> "Webpage Management for Business Owners",
+	"webpage_description2" 	=> "and One-Stop Portal Costumers",
+	"search_for"			=> "SEARCH FOR",
+	"best_business"			=> "THE BEST BUSINESS",
+	"for_you"				=> "FOR YOU",
+
+	/* Featured business */
+	'see_more'				=> "See more",
+
+	/* Second banner */
+	'add_your_business'		=> "Add your business",
+	'your_business_name_msg' => "Your business name could be here!",
+	'learn_how'				=> "Learn how",
+	'we_make_it_easy_msg'	=> "We Make it Easy for Both Businesses and Customers", 
+	'have_you_been_frustrated' => "Have you been frustrated by the lack of businesses with webpages around Lakeside? We list businesses and manage their pages to ensure they are up to date, and all in one convenient location. You can search for businesses by category or look up a specific business to view their own external page or their professionally managed webpage through our portal.",
+	'sign_up_today'			=> "Sing Up Your Business Today!",
+
+	/* Footer */
+	'product_by' 				=> "An Alex Searles and Ajijic Business Enterprises Ltd. Product",
+	'images_by'					=> "Images by"
+];
---
+++ resources/lang/es/general.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+	/* === HOME PAGE ===*/
+	/* Main menu */
+	'home' => 'Inicio',
+	'business_categories' 	=> "Categorias",
+	'list_your_business'  	=> "Enlista tu negocio",
+	'about_us'				=> "Acerca de",
+	'contact'				=> 'Contácto',
+	'search'				=> 'Búscar',
+
+	/* Main banner */
+	"webpage_description" 	=> "Gestión de páginas web para propietarios de negocios",
+	"webpage_description2" 	=> "y clientes del Portal",
+	"search_for"			=> "BUSCA",
+	"best_business"			=> "EL MEJOR NEGOCIO",
+	"for_you"				=> "PARA TI",
+
+	/* Featured business */
+	'see_more'				=> "Ver más",
+
+	/* Second banner */
+	'add_your_business'		=> "AGREGA TU NEGOCIO",
+	'your_business_name_msg' => "TU NEGOCIO PUEDE ESTAR AQUÍ!",
+	'learn_how'				=> "Revisa cómo",
+	'we_make_it_easy_msg'	=> "Lo hacemos fácil para negocios y clientes",
+	'have_you_been_frustrated' => "¿Se ha sentido frustrado por la falta de empresas con páginas web en el área del Lago de Chapala? Enumeramos empresas y administramos sus páginas web para asegurarnos de que estén actualizadas y todo en una ubicación conveniente. Puede buscar empresas por categoría o buscar una empresa específica para ver su propia página externa o su página web administrada profesionalmente a través de nuestro portal.",
+	'sign_up_today'			=> "¡Registre su negocio hoy!",
+
+	/* Footer */
+	'product_by' 				=> "Un producto de Alex Searles and Ajijic Business Enterprises Ltd.",
+	'images_by'					=> "Imagenes provistas por"
+];
