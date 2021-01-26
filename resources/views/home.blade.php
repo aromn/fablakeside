6c8a58127a2be2ee7bcbeed6589f280db37a7d5a
Aldo Romero <aldoromnn@gmail.com>
2021-01-26 14:04:55 -0600

    Views => home: Removed featured businesses tiles and added search section.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,222 +20,115 @@
 
 <!-- ======= SPECIAL BUSINESS SECTION ======= -->
 <section class="section site-portfolio">  
-    <div class="container menuPortfolio">
+<!--SEARCH BAR WITH CATEGORIES AND SUB CATEGORIES-->
+<div class="container pt-4">
+  <div class="row">
+    <div class="col-12">
+       <form type="get" action="{{ route('search', app()->getLocale()) }}">
+          
 
-        <!--CATEGORIES SELECTION-->
-        <div class="row mb-5 align-items-center categoriesSection">
-            <div class="col-md-12 col-lg-12 text-center text-lg-center">
-                <div id="filters" class="filters">
-                    <a href="#" data-filter="*" class="active">ALL</a>
-                    <a href="#" data-filter=".restaurants">RESTAURANTS</a>
-                    <a href="#" data-filter=".retail">RETAIL</a>
-                    <a href="#" data-filter=".services">SERVICES</a>
-                    <a href="#" data-filter=".bars">BARS & CANTINAS</a>
+          <!-- MORE SPECIFIC SEARCH OPTIONS -->
+          <div class="row py-2">
+              <div class="col-md-4">
+                  <div class="form-group">
+                      <select name="location" class="form-control" id="sel2">
+                          <option value="">{{ __('general.select_location') }}</option>
+                          <option value="Ajijic">Ajijic</option>
+                          <option value="Chapala">Chapala</option>
+                          <option value="Ixtlahuacan">Ixtlahuacán de los M.</option>
+                          <option value="San Antonio">San Antonio</option>
+                    </select>
+                  </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="form-group">
+                    <select name="category" class="form-control" id="categories">
+                        <option value="">{{ __('general.select_category') }}</option>
+                        @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                  </select>
                 </div>
             </div>
-        </div>
-        <!--END CATEGORIES SELECTION-->
 
-        <!--BUSINESS PICTURE SECTION-->
-        <div id="portfolio-grid" class="row no-gutter" ><!--data-aos="fade-up" data-aos-delay="200"-->
-        <!--The Fade In Data AOS effect doesn't work, don't know why-->
-
-            <!-- COLUMN 1 | 4 ITEMS-->
-
-            <!--Casa Linda Restaurante-->
-            <div class="item restaurants col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijicfoodandbev.com/casa-linda" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Casa Linda Restaurante</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_0.jpg">
-                </a>
-            </div>
-
-            <!--Deli Very Deli-->
-            <div class="item restaurants col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijicfoodandbev.com/deli-very-deli" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Deli-Very Deli</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_13.jpeg">
-                </a>
-            </div>
-
-            <!--The Art House-->
-            <div class="item retail shopping col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijiclisted.com/the-art-house" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>The Art House</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_12.jpg">
-                </a>
-            </div>
-
-            <!--Casita de Muñecas-->
-            <div class="item retail col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijiclisted.com/casita-de-mu" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Casita de Muñecas</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_11.jpg">
-                </a>
-            </div>
-
-            <!-- END COLUMN 1 | 4 ITEMS-->
-
-
-            <!-- COLUMN 2 | 4 ITEMS-->
-
-            <!--Professional Massages-->
-            <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijiclisted.com/professional-massages-of-lakeside" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Professional Massages</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_10.jpg">
-                </a>
-            </div>
-
-            <!--Le Club 4 / Oui Ou-->
-            <div class="item restaurants bars col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="#" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Le Club 4 / Oui Oui</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_14.jpeg">
-                </a>
-            </div>
-
-            <!--Rolo Hair-->
-            <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijiclisted.com/rolo-hair" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Rolo Hair</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_9.jpg">
-                </a>
-            </div>
-
-            <!--Fusion Tactical-->
-            <div class="item retail col-sm-6 col-md-4 col-lg-3 mb-4">
-                <a href="https://www.fabajijiclisted.com/fusion-tactica" class="item-wrap fancybox">
-                    <div class="work-info">
-                        <h3>Fusion Tactical</h3>
-                        <span>{{ __('general.see_more') }}</span>
-                    </div>
-                    <img class="img-fluid" src="img/principalBusinessImg/img_8.jpg">
-                </a>
-            </div>
-
-        <!-- END COLUMN 2 | 4 ITEMS-->
-
-
-        <!-- COLUMN 3 | 4 ITEMS-->
-
-        <!-- Media Luna -->
-        <div class="item restaurants healthCare col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijicfoodandbev.com/media-luna" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Media Luna</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_7.jpg">
-            </a>
-        </div>
-
-        <!-- Laundry -->
-        <div class="item services house col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijiclisted.com/la-princesa" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Mi Princesa Laundry</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_5.jpg">
-            </a>
-        </div>
-
-        <!-- Gardening Services -->
-        <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijiclisted.com/andres-gardening" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Gardening Services</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_6.jpg">
-            </a>
-        </div>
-
-        <!-- Laundry Amigos -->
-        <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijiclisted.com/lavanderia-y-tintoreria-amigos" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Laundry Amigos</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_15.jpeg">
-            </a>
-        </div>
-
-        <!-- END COLUMN 3 | 4 ITEMS-->
-
-        <!-- COLUMN 4 OF 4 ITEMS-->
-
-        <!-- Meson de Don Quijote -->
-        <div class="item restaurants col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijicfoodandbev.com/meson-don-quijote" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Meson de Don Quijote</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_4.jpg">
-            </a>
-        </div>
-
-        <!-- Mojo Dental -->
-        <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijiclisted.com/mojo-dental" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Mojo Dental</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_3.jpg">
-            </a>
-        </div>
-
-        <!-- Casa Catrina -->
-        <div class="item retail col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.fabajijiclisted.com/casa-de-catrina" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Casa Catrina</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_2.jpg">
-            </a>
-        </div>
-
-        <!-- Luxury Homes -->
-        <div class="item services col-sm-6 col-md-4 col-lg-3 mb-4">
-            <a href="https://www.luxuryhomesajijic.com/properties" class="item-wrap fancybox">
-                <div class="work-info">
-                    <h3>Luxury Homes</h3>
-                    <span>{{ __('general.see_more') }}</span>
-                </div>
-                <img class="img-fluid" src="img/principalBusinessImg/img_1.jpg">
-            </a>
-        </div>
-
-      <!-- END COLUMN 4 OF 4 ITEMS-->
+            <div class="col-md-4">
+              <div class="form-group">
+                  <select name="subcategory" class="form-control" id="subcategories">
+                      <option value="" default>{{ __('general.select_subcategory') }}</option>
+                  </select>
+              </div>
+          </div>
 
         </div>
+        <!-- MORE SPECIFIC SEARCH OPTIONS -->
+
+        <!--SEARCH BAR-->
+        <div id="custom-search-input" class="row py-2">
+          <div class="col-xs-12 col-sm-12 col-lg-10">
+              <input type="search" name="query" class="search-query form-control" placeholder="{{ __('general.search') }}"/>
+          </div>
+          <div class="d-block d-sm-none">&nbsp;</div>
+          <div class="col-xs-12 col-sm-12 col-lg-2 text-sm-center search">
+              <button class="btn btn-primary" type="submit" style="width: 100%">{{ __('general.search') }}</button>
+          </div>
+      </div>
+      <!-- END SEARCH BAR-->
+          
+          
+        <script>
+
+          $(document).ready(function(){
+              if (window.location.toString().includes("/en/") || window.location.toString().includes("/en")) {
+                $('.lang-es').removeClass('btn-outline-primary');
+                $('.lang-es').addClass('btn-outline-secondary');
+                $('.lang-en').removeClass('btn-outline-secondary');
+                $('.lang-en').addClass('btn-outline-primary');
+              } 
+              else if (window.location.toString().includes("/es/") || window.location.toString().includes("/es")) {
+                $('.lang-en').removeClass('btn-outline-primary');
+                $('.lang-en').addClass('btn-outline-secondary');
+                $('.lang-es').removeClass('btn-outline-secondary');
+                $('.lang-es').addClass('btn-outline-primary');
+              } 
+          });
+
+
+            $('#categories').change(function(){
+              if($(this).val() == ""){
+                $('#subcategories option').each(function(){
+                    $(this).remove();
+                  });
+                $('#subcategories').append('<option value="">Select a subcategory</option>');
+                $('#subcategories').prop("disabled", true);
+                        return false;
+                  }
+
+          $('#subcategories option').each(function(){
+            $(this).remove();
+          });
+
+          $.ajax({
+              url: '/subcategories/' + $(this).val(),
+              type: 'GET',
+              success: function(e){
+                $('#subcategories').removeAttr("disabled");
+                $('#subcategories').append('<option value="">Select a subcategory</option>');
+                  $.each(e, function(index, value){
+                      console.log(value);
+                              $('#subcategories').append('<option value="' + value.id + '">' + value.name + '</option>');
+                  });
+              }
+             });
+        });
+        </script>
+
+
+      </form>
     </div>
+  </div>
+</div>
+<!--SECTION SEARCH-->
 </section>
 <!-- End  Business Section -->
 
